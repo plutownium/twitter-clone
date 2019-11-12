3882de1d31637076640ec44988962e6456e2a74b
plutownium <rolandmackintosh@gmail.com>
2019-11-11 18:33:39 -0800

    refactored routes.js to match tutorial; seems ok

--- src/routes.js
+++ src/routes.js
@@ -5,26 +5,33 @@ import Homepage from "./components/Homepage.vue";
 import TweetDeck from "./components/TweetDeck.vue";
 import Login from "./components/Login.vue";
 import Signup from "./components/Signup.vue";
-import VueRouter from "vue-router";
+
+import Router from "vue-router";
 import firebase from "firebase";
 
-export default [
-  { path: "*", redirect: "/" },
-  { path: "/", name: "Home", component: Homepage },
-  { path: "/messages", name: "DMs", component: Messages },
-  { path: "/settings", name: "Settings", component: Settings },
-  {
-    path: "/tweet",
-    name: "Tweet",
-    component: TweetDeck,
-    meta: { requiresAuth: true }
-  },
-  { path: "/tweets/:id", name: "SingleTweet", component: SingleTweet },
-  { path: "/login", name: "Login", component: Login },
-  { path: "/signup", name: "Signup", component: Signup }
-];
+import Vue from "vue";
 
-VueRouter.beforeEach((to, from, next) => {
+Vue.use(Router);
+
+const router = new Router({
+  routes: [
+    { path: "*", redirect: "/" },
+    { path: "/", name: "Home", component: Homepage },
+    { path: "/messages", name: "DMs", component: Messages },
+    { path: "/settings", name: "Settings", component: Settings },
+    {
+      path: "/tweet",
+      name: "Tweet",
+      component: TweetDeck,
+      meta: { requiresAuth: true }
+    },
+    { path: "/tweets/:id", name: "SingleTweet", component: SingleTweet },
+    { path: "/login", name: "Login", component: Login },
+    { path: "/signup", name: "Signup", component: Signup }
+  ]
+});
+
+router.beforeEach((to, from, next) => {
   const currentUser = firebase.auth().currentUser;
   const requiresAuth = to.matched.some(record => record.meta.requiresAuth);
   // "if the page requires auth and there is no current user, redirect to the login page"
@@ -34,3 +41,5 @@ VueRouter.beforeEach((to, from, next) => {
   // "otherwise, load next page"
   else next();
 });
+
+export default router;
