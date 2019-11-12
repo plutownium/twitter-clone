864bbc132b25a46652bf2709657c43a4237def5f
plutownium <rolandmackintosh@gmail.com>
2019-11-11 18:29:26 -0800

    broken; Uncaught TypeError in routes.js

--- src/routes.js
+++ src/routes.js
@@ -5,8 +5,11 @@ import Homepage from "./components/Homepage.vue";
 import TweetDeck from "./components/TweetDeck.vue";
 import Login from "./components/Login.vue";
 import Signup from "./components/Signup.vue";
+import VueRouter from "vue-router";
+import firebase from "firebase";
 
 export default [
+  { path: "*", redirect: "/" },
   { path: "/", name: "Home", component: Homepage },
   { path: "/messages", name: "DMs", component: Messages },
   { path: "/settings", name: "Settings", component: Settings },
@@ -20,3 +23,14 @@ export default [
   { path: "/login", name: "Login", component: Login },
   { path: "/signup", name: "Signup", component: Signup }
 ];
+
+VueRouter.beforeEach((to, from, next) => {
+  const currentUser = firebase.auth().currentUser;
+  const requiresAuth = to.matched.some(record => record.meta.requiresAuth);
+  // "if the page requires auth and there is no current user, redirect to the login page"
+  if (requiresAuth && !currentUser) next("login");
+  // "if the page doesn't require auth and there is a current user, go to the home page"
+  else if (!requiresAuth && currentUser) next("home");
+  // "otherwise, load next page"
+  else next();
+});
